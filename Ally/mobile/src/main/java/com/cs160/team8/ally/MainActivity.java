b5d0d5b1e6f9d7d4f8f8f3f935fd618e83ff5dff
Kunal Patel <kunaldpatel95@gmail.com>
2016-04-15 16:08:10 -0700

    Added Kunal's phoneListenerService classes; not working yet

--- Ally/mobile/src/main/java/com/cs160/team8/ally/MainActivity.java
+++ Ally/mobile/src/main/java/com/cs160/team8/ally/MainActivity.java
@@ -3,24 +3,15 @@ package com.cs160.team8.ally;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -77,6 +68,16 @@ public class MainActivity extends AppCompatActivity
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
         setupTabIcons();
+
+
+        /*
+
+        HARD-CODED PROFILE PUSH
+
+
+
+
+         */
     }
 
     private void setupTabIcons() {
--- Ally/mobile/src/main/java/com/cs160/team8/ally/PhoneToWatchService.java
+++ Ally/mobile/src/main/java/com/cs160/team8/ally/PhoneToWatchService.java
@@ -1,103 +1,125 @@
 package com.cs160.team8.ally;
 
-import android.app.Service;
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.text.TextUtils;
+import android.util.Log;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 /**
  * Created by joleary on 2/19/16.
  */
-public class PhoneToWatchService extends Service {
+public class PhoneToWatchService extends WearableListenerService implements GoogleApiClient.ConnectionCallbacks {
 
-    private GoogleApiClient mApiClient;
-    private static final String SENATOR_INFO = "/SENATOR_INFO";
+    private static GoogleApiClient mApiClient;
+    private  String path;
+    private  byte[] message;
+    private Context context;
+
+    public PhoneToWatchService() {
+        this.path = "sample";
+        this.message = "sample".getBytes();
+
+    }
+
+    public PhoneToWatchService(String path, String message) {
+        this.path = path;
+        this.message = message.getBytes();
+    }
+    public PhoneToWatchService(String path, byte[] message) {
+        this.path = path;
+        this.message = message;
+    }
+
+
+    public static int byteArrayToLeInt(byte[] b) {
+        final ByteBuffer bb = ByteBuffer.wrap(b);
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        return bb.getInt();
+    }
+
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        //initialize the googleAPIClient for message passing
-        mApiClient = new GoogleApiClient.Builder( this )
-                .addApi( Wearable.API )
-                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-                    @Override
-                    public void onConnected(Bundle connectionHint) {
-                    }
+    public void onMessageReceived(MessageEvent messageEvent) {
+        System.out.println("------------------message received by phone------------------------");
+        path = messageEvent.getPath();
+        message = messageEvent.getData();
 
-                    @Override
-                    public void onConnectionSuspended(int cause) {
-                    }
-                })
-                .build();
+    }
+
+    public void sendMessage(Context givenContext) {
+        this.context = givenContext;
+        mApiClient = new GoogleApiClient.Builder(givenContext).addApi(Wearable.API).addConnectionCallbacks(this).build();
+        mApiClient.connect();
+
+    }
+
+    public void sendMessage(final String newPath, final String text, final Context context) {
+        Log.d("SENDING", "MESSAGE");
+        Log.d("SENDING", "MESSAGE");
+
+        setPath(path);
+        setMessage( text.getBytes());
+
+        mApiClient = new GoogleApiClient.Builder(context).addApi(Wearable.API).addConnectionCallbacks(new PhoneToWatchService(path, message)).build();
+        mApiClient.connect();
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mApiClient.disconnect();
-    }
+    public void onConnected(Bundle bundle) {
+        System.out.println("Connected");
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent != null && intent.getExtras() != null) {
-            Bundle extras = intent.getExtras();
 
-            String[] names = extras.getStringArrayList(ListRepresentativesActivity.NAMES_KEY).toArray(new String[0]);
-            String[] parties = extras.getStringArrayList(ListRepresentativesActivity.PARTIES_KEY).toArray(new String[0]);
-            String location = extras.getString(ListRepresentativesActivity.LOCATION_KEY);
-            double obamaVotes = extras.getDouble(ListRepresentativesActivity.OBAMA_KEY);
-            double romneyVotes = extras.getDouble(ListRepresentativesActivity.ROMNEY_KEY);
+        Log.d("SENDING", "MESSAGE");
 
-            String namePart = TextUtils.join(":", names);
-            String partyPart = TextUtils.join(":", parties);
-            String locationPart = TextUtils.join(":", new String[] { location, String.valueOf(obamaVotes), String.valueOf(romneyVotes) });
-            String[] parts = new String[]{ namePart, partyPart, locationPart };
-            final String senatorInfo = TextUtils.join("+", parts);
 
-            // Send the message with the cat name
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    //first, connect to the apiclient
-                    mApiClient.connect();
-                    //now that you're connected, send a massage with the cat name
-                    sendMessage(SENATOR_INFO, senatorInfo);
-                }
-            }).start();
-        }
+        System.out.println(" I'm about to send " + this.path);
+        System.out.println(" I'm about to send "  + new String(message));
 
-        return START_STICKY;
-    }
 
-    @Override //remember, all services need to implement an IBiner
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    private void sendMessage( final String path, final String text ) {
-        //one way to send message: start a new thread and call .await()
-        //see WatchToPhoneService for another way to send a message
         new Thread( new Runnable() {
             @Override
             public void run() {
                 NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi.getConnectedNodes( mApiClient ).await();
                 for(Node node : nodes.getNodes()) {
-                    //we find 'nodes', which are nearby bluetooth devices (aka emulators)
-                    //send a message for each of these nodes (just one, for an emulator)
                     MessageApi.SendMessageResult result = Wearable.MessageApi.sendMessage(
-                            mApiClient, node.getId(), path, text.getBytes() ).await();
-                    //4 arguments: api client, the node ID, the path (for the listener to parse),
-                    //and the message itself (you need to convert it to bytes.)
+                            mApiClient, node.getId(), path, message ).await();
                 }
+                mApiClient.disconnect();
             }
         }).start();
+
+        System.out.println("Sent");
     }
 
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+
+    public String getPath() {
+        return path;
+    }
+
+    public byte[] getMessage() {
+        return message;
+    }
+
+    public void setPath(final String newPath) {
+        this.path = newPath;
+    }
+
+    public void setMessage(final byte[] newMessage) {
+        this.message = newMessage;
+    }
 }
