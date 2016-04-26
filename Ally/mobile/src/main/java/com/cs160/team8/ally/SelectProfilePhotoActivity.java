16ae63968f5132c5ba23d62d4ccc21408f7e9ad1
Joel Graycar <jgraycar@berkeley.edu>
2016-04-26 12:16:41 -0700

    Update app icon, appbar styling

--- Ally/mobile/src/main/java/com/cs160/team8/ally/SelectProfilePhotoActivity.java
+++ Ally/mobile/src/main/java/com/cs160/team8/ally/SelectProfilePhotoActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SelectProfilePhotoActivity extends AppCompatActivity {
 
@@ -11,6 +13,17 @@ public class SelectProfilePhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_profile_photo);
+
+        Window window = getWindow();
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.setStatusBarColor(getResources().getColor(R.color.black));
+
         setTitle("Add Photo");
         Toolbar myToolbar = (Toolbar) findViewById(R.id.select_profile_photo_toolbar);
         setSupportActionBar(myToolbar);
